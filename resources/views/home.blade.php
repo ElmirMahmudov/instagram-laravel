8e6ed9a17d2a905d924cf48b23bfe7388eb6b200
ElmirMahmudov <elmir9496@gmail.com>
2019-06-18 22:08:56 +0400

    username field added to user table user

---
+++ resources/views/home.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-3">
+            <img src="https://instagram.fgyd10-1.fna.fbcdn.net/vp/7ae44014df5cb0416b313a2d90fde2e5/5D85E047/t51.2885-19/s150x150/61293395_425831504663296_6025471149259882496_n.jpg?_nc_ht=instagram.fgyd10-1.fna.fbcdn.net"
+                alt=""
+                class="rounded-circle"
+                >
+        </div>
+        <div class="col-md-9">
+            <div class="row">
+                <div class="col-12">
+                    <h1>
+                        Name surname
+                    </h1>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-3">
+                    <b>15 </b>
+                </div>
+                <div class="col-3">
+                    <b>123</b>
+                </div>
+                <div class="col-3">
+                    <b>322</b>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <b>Farid Talybly | Sport Blogger</b>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <span>Goals are only important ,if they win games</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="#">youtu.be/8L2HNjI0PLs</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row pt-5">
+        <div class="imgCont col-4">
+            <img
+            class="w-100"
+            src="https://instagram.fgyd10-1.fna.fbcdn.net/vp/479afc8228020b7a3c89aeccfe6d8435/5DA23D7F/t51.2885-15/sh0.08/e35/c0.180.1440.1440/s640x640/58613112_121018602429748_7577382688467894564_n.jpg?_nc_ht=instagram.fgyd10-1.fna.fbcdn.net" alt="">
+        </div>
+        
+        <div class="imgCont col-4">
+            <img
+            class="w-100"
+            src="https://instagram.fgyd10-1.fna.fbcdn.net/vp/479afc8228020b7a3c89aeccfe6d8435/5DA23D7F/t51.2885-15/sh0.08/e35/c0.180.1440.1440/s640x640/58613112_121018602429748_7577382688467894564_n.jpg?_nc_ht=instagram.fgyd10-1.fna.fbcdn.net" alt="">
+        </div>
+        
+        <div class="imgCont col-4">
+            <img
+            class="w-100"
+            src="https://instagram.fgyd10-1.fna.fbcdn.net/vp/479afc8228020b7a3c89aeccfe6d8435/5DA23D7F/t51.2885-15/sh0.08/e35/c0.180.1440.1440/s640x640/58613112_121018602429748_7577382688467894564_n.jpg?_nc_ht=instagram.fgyd10-1.fna.fbcdn.net" alt="">
+        </div>
+    </div>
+</div>
+@endsection
